e5cdfb3b3c66159d5ea399a9d2c6e2996355a8a7
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-05-09 17:47:07 +0700

    add metadata props and openGraph props, add custom openGraph image made with figma

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,35 @@ export const metadata = {
 	title: "pcnamegen",
 	description:
 		"A simple name generator for your computer who recently installed/reinstalled OS",
+	metadataBase: "https://pcnamegen.vercel.app",
+	robots: {
+		index: true,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: true,
+		},
+		bingBot: {
+			index: true,
+			follow: true,
+		},
+	},
+	openGraph: {
+		title: "pcnamegen",
+		description:
+			"A simple name generator for your computer who recently installed/reinstalled OS",
+		url: "https://pcnamegen.vercel.app",
+		siteName: "pcnamegen",
+		images: [
+			{
+				url: "/pcnamegen-ogimage.png",
+				width: 1200,
+				height: 630,
+				alt: "pcnamegen OpenGraph Image, made with Figma and Bootstrap Icons",
+			},
+		],
+	},
 };
 
 export default function RootLayout({ children }) {
